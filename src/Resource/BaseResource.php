b99df26438425201ec4e2371844da4fa3388fe91
Andis Cirulis <andis.cirulis@whitedigital.eu>
2022-05-09 18:24:45 +0300

    Small refactor, adding $id as required property in base class.

--- src/Resource/BaseResource.php
+++ src/Resource/BaseResource.php
@@ -10,6 +10,7 @@ use WhiteDigital\EntityResourceMapper\Mapper\EntityToResourceMapper;
 
 abstract class BaseResource
 {
+    public ?int $id = null;
 
     private static EntityToResourceMapper $entityToResourceMapper;
 
@@ -22,17 +23,19 @@ abstract class BaseResource
     {
         self::$entityToResourceMapper = $mapper;
     }
-    
+
     /**
-     * Will create a Resource from Entity, by using EntityToResourceMapper
+     * Factory method to create a Resource from Entity, by using EntityToResourceMapper
+     * @param BaseEntity $entity
+     * @param array<string> $context
+     * @return static
      * @throws ExceptionInterface
      * @throws ResourceClassNotFoundException
-     * @throws \Exception
      */
     public static function create(BaseEntity $entity, array $context): static
     {
         $resource = self::$entityToResourceMapper->map($entity, $context);
-        if (!$resource instanceof static) {
+        if (!($resource instanceof static)) {
             throw new \RuntimeException(sprintf("Wrong type (%s instead of %s) in Resource factory.", get_class($resource), static::class));
         }
         return $resource;
