7e713e7755d9c451997deb866b6aa5a8e8e89192
Davis Zalitis <davis.zalitis@whitedigital.eu>
2023-02-07 12:09:14 +0200

    refactor: use abstract bundle configuration

--- src/EntityResourceMapperBundle.php
+++ src/EntityResourceMapperBundle.php
@@ -2,14 +2,21 @@
 
 namespace WhiteDigital\EntityResourceMapper;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 use function dirname;
 
-class EntityResourceMapperBundle extends Bundle
+class EntityResourceMapperBundle extends AbstractBundle
 {
     public function getPath(): string
     {
         return dirname(__DIR__);
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
     }
 }
